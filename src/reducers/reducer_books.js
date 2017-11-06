4c12dbcc6e93226bfadea7b6d8fa95b9b1322516
irhamputra <irhamputraprasetyo@gmail.com>
2017-11-06 03:25:01 +0100

    final code playing with Redux

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        { title: 'JS: The Good Part', pages: 101 },
+        { title: 'Harry Potter', pages: 59 },
+        { title: 'The Dark Knight', pages: 66 },
+        { title: 'Eloquent Ruby', pages: 8 },
+    ]
+}
